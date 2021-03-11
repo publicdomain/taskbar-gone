e65e8c6faa07f9f35c3df521f9634e59c170d970
Victor L. Senior <publicdomainvic@gmail.com>
2021-03-11 19:21:02 -0400

    Add OnEnableDisableButtonClick() code

--- TaskbarGone/MainForm.cs
+++ TaskbarGone/MainForm.cs
@@ -18,6 +18,46 @@ namespace TaskbarGone
     public partial class MainForm : Form
     {
         /// <summary>
+        /// The taskbar handle.
+        /// </summary>
+        IntPtr taskbarHandle;
+
+        /// <summary>
+        /// The start button handle.
+        /// </summary>
+        IntPtr startButtonHandle;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, SetWindowPosFlags uFlags);
+
+        [DllImport("user32", EntryPoint = "FindWindowA", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindowEx(IntPtr parentHwnd, IntPtr childAfterHwnd, IntPtr className, string windowText);
+
+        [Flags]
+        private enum SetWindowPosFlags : uint
+        {
+            SWP_ASYNCWINDOWPOS = 0x4000,
+            SWP_DEFERERASE = 0x2000,
+            SWP_DRAWFRAME = 0x0020,
+            SWP_FRAMECHANGED = 0x0020,
+            SWP_HIDEWINDOW = 0x0080,
+            SWP_NOACTIVATE = 0x0010,
+            SWP_NOCOPYBITS = 0x0100,
+            SWP_NOMOVE = 0x0002,
+            SWP_NOOWNERZORDER = 0x0200,
+            SWP_NOREDRAW = 0x0008,
+            SWP_NOREPOSITION = 0x0200,
+            SWP_NOSENDCHANGING = 0x0400,
+            SWP_NOSIZE = 0x0001,
+            SWP_NOZORDER = 0x0004,
+            SWP_SHOWWINDOW = 0x0040,
+        }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="T:TaskbarGone.MainForm"/> class.
         /// </summary>
         public MainForm()
@@ -33,7 +73,29 @@ namespace TaskbarGone
         /// <param name="e">Event arguments.</param>
         private void OnEnableDisableButtonClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // TODO Set handles [Set in another function]
+            this.taskbarHandle = FindWindow("Shell_traywnd", string.Empty);
+            this.startButtonHandle = FindWindowEx(IntPtr.Zero, IntPtr.Zero, (IntPtr)0xC017, null);
+
+            // Act om button text
+            if (this.enableDisableButton.Text.StartsWith("&E", StringComparison.InvariantCulture))
+            {
+                // Hide
+                SetWindowPos(this.taskbarHandle, IntPtr.Zero, 0, 0, 0, 0, SetWindowPosFlags.SWP_HIDEWINDOW);
+                SetWindowPos(this.startButtonHandle, IntPtr.Zero, 0, 0, 0, 0, SetWindowPosFlags.SWP_HIDEWINDOW);
+
+                // Update button text
+                this.enableDisableButton.Text = "&Disable";
+            }
+            else
+            {
+                // Show 
+                SetWindowPos(this.taskbarHandle, IntPtr.Zero, 0, 0, 0, 0, SetWindowPosFlags.SWP_SHOWWINDOW);
+                SetWindowPos(this.startButtonHandle, IntPtr.Zero, 0, 0, 0, 0, SetWindowPosFlags.SWP_SHOWWINDOW);
+
+                // Update button text
+                this.enableDisableButton.Text = "&Enable";
+            }
         }
 
         /// <summary>
